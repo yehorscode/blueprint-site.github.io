58dc6b1632fca1ff1c0bd1fd146c7a54f05079d3
blueprint-site <168190209+blueprint-site@users.noreply.github.com>
2024-04-26 13:46:07 +0200

    Add files via upload

---
+++ data/dict.py
@@ -0,0 +1,3 @@
+addon_list = {
+    1: ["Create: Structures", "IAnP4np7", None, "https://modrinth.com/datapack/create-structures", None, "https://cdn.modrinth.com/data/IAnP4np7/0c69bfc4fb1df010c35c02f94ebc275ba0fa9bae.png", "195.4k", None, ["1.19.2", "1.20.1"], ["Fabric", "Forge", "Datapack"], "#B3CAE5"]
+}
